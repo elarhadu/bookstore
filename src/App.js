825a48b500b10c60a4960643f7ba76c8a9c0f3e9
elarhadu <elarhadu@gmail.com>
2023-01-27 11:43:49 +0000

    Move Provider and store to index.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/configureStore';
 import Navigation from './Components/Navigation';
 import Books from './Pages/Books';
 import Categories from './Pages/Categories';
@@ -10,14 +8,14 @@ import './App.css';
 export default function App() {
   return (
     <Router>
-      <Provider store={store}>
-        <Navigation />
-        <h1>BOOKSTORE📖</h1>
-        <Routes>
-          <Route path="/" element={<Books />} />
-          <Route path="/categories" element={<Categories />} />
-        </Routes>
-      </Provider>
+
+      <Navigation />
+      <h1>BOOKSTORE📖</h1>
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+
     </Router>
   );
 }
